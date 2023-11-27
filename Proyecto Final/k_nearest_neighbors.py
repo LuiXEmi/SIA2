ba38fd73c21339fa5741dd8d11e28149639d5ff0
Luis Emiliano <62229364+LuiXEmi@users.noreply.github.com>
2023-11-26 19:57:04 -0600

    Add files via upload

---
+++ Proyecto Final/k_nearest_neighbors.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+
+# Cargar los datos desde un archivo CSV
+file_path = "zoo.csv"  # Reemplaza esto con la ruta correcta
+df = pd.read_csv(file_path)
+
+# Dividir los datos en características (X) y etiquetas (y)
+X = df.iloc[:, 1:-1].values
+y = df.iloc[:, -1].values
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Inicializar y entrenar el modelo de KNN
+knn_model = KNeighborsClassifier(n_neighbors=3)  # Puedes ajustar el valor de 'n_neighbors'
+knn_model.fit(X_train, y_train)
+
+# Realizar predicciones en el conjunto de prueba
+y_pred = knn_model.predict(X_test)
+
+# Evaluar el rendimiento del modelo
+# Evaluar el rendimiento del modelo
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='weighted', zero_division=1)
+recall = recall_score(y_test, y_pred, average='weighted', zero_division=1)
+f1 = f1_score(y_test, y_pred, average='weighted', zero_division=1)
+
+
+# Calcular la matriz de confusión
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(conf_matrix)
+
+# Imprimir las métricas
+print(f"Accuracy: {accuracy}")
+print(f"Precision: {precision}")
+print(f"Recall (Sensitivity): {recall}")
+print(f"F1 Score: {f1}")
+
---
+++ Proyecto Final/svm.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+
+# Cargar los datos desde un archivo CSV
+file_path = "zoo.csv"  # Reemplaza esto con la ruta correcta
+df = pd.read_csv(file_path)
+
+# Dividir los datos en características (X) y etiquetas (y)
+X = df.iloc[:, 1:-1].values
+y = df.iloc[:, -1].values
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Inicializar y entrenar el modelo SVM
+svm_model = SVC(kernel='linear')  # Puedes ajustar el tipo de kernel según tus necesidades
+svm_model.fit(X_train, y_train)
+
+# Realizar predicciones en el conjunto de prueba
+y_pred = svm_model.predict(X_test)
+
+# Evaluar el rendimiento del modelo
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='weighted', zero_division=1)
+recall = recall_score(y_test, y_pred, average='weighted', zero_division=1)
+f1 = f1_score(y_test, y_pred, average='weighted', zero_division=1)
+
+# Calcular la matriz de confusión
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(conf_matrix)
+
+# Imprimir las métricas
+print(f"Accuracy: {accuracy}")
+print(f"Precision: {precision}")
+print(f"Recall (Sensitivity): {recall}")
+print(f"F1 Score: {f1}")
